7aa7316dfd9dc1e876a77d595d3316dfa86b8f00
Alvin Merto <ajbmerto@gmail.com>
2023-11-12 14:07:46 +0800

    first commit

---
+++ app/Http/Controllers/BudgetSystemController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BudgetSystemController extends Controller
+{
+    //
+    public function budgetsystem(Request $req) {
+        echo "hello world";
+    }
+}
